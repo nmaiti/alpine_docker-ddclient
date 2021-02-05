a18eb33a5fe2ace76ec905cf0932a1bcb6da6e00
Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>
2021-02-05 10:57:27 +0530

    Update ci.sh
    Add linux/ppc64le,linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
    
    Update ci.sh
    
    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,32 +1,32 @@
-#!/bin/bash 
+#!/bin/bash
 
 ###################################################################
 # Script Name : ci.sh
-# 
+#
 # Description :
 #
-# Args :
-#
+# Args :  docker buildx build \
+    #
 # Creation Date : 28-11-2020
 # Last Modified :
-# 
-# Created By :  
+#
+# Created By :
 ###################################################################
 #!/bin/bash
 # removed ppc64le temoporaryly as alpine repo is bad now
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
-  docker buildx build \
-    --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7\
-    .
-  exit $?
+    docker buildx build \
+        --progress plain \
+        --platform=linux/386,linux/amd64,linux/arm/v7,linux/arm/v6,linux/arm64,linux/s390x \
+        .
+    exit $?
 fi
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERID --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
-     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7 \
+    --progress plain \
+    --platform=linux/386,linux/amd64,linux/arm/v7,linux/arm/v6,linux/arm64,linux/s390x \
     -t $DOCKER_USERID/$DOCKER_REPO:$TAG \
     --push \
     .
